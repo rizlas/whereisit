91cd5f6835170aa9f538e3b7d465bafa11dedab8
rizlas <fonzy9114@gmail.com>
2019-12-03 15:33:58 +0100

    Enabled error handler

--- bot.py
+++ bot.py
@@ -398,7 +398,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, non_command))
 
     # log all errors
-    # dp.add_error_handler(error)
+    dp.add_error_handler(error)
 
     run(updater)
 
